b3f94dc62f8b3c9214385ce91a62a5dc82218662
João H M Santos <55803774+jhmonchelato@users.noreply.github.com>
2023-06-22 16:57:03 -0300

    atualização com melhorias

--- pub-sub.py
+++ pub-sub.py
@@ -2,20 +2,25 @@ import zmq
 import time
 import threading
 
-def publisher():
+global client_id
+
+def publisher(nick):
     porta = input("Digite a a porta que deseja conectar: ")
+    portas = porta.split()
     context = zmq.Context()
     socket = context.socket(zmq.PUB)
-    socket.connect(f"tcp://localhost:{porta}")
+    for p in portas:
+        socket.connect(f"tcp://localhost:{p}")
     
 
     while True:
         message = input("Digite uma mensagem: ")
+        message = f'{nick}: {message}'
         socket.send_string(message)
         
 
-def subscriber():
-    porta = input("Digite a sua porta: ")
+def subscriber(porta):
+    
     context = zmq.Context()
     socket = context.socket(zmq.SUB)
     socket.bind(f"tcp://*:{porta}")
@@ -23,14 +28,16 @@ def subscriber():
 
     while True:
        message = socket.recv_string()
-       print(f"Mensagem recebida: {message}")
+       print(message)
          
 
+porta, nick = input("Digite a sua porta e seu nick: ").split()
+
 # Iniciar o subscritor
-threading.Thread(target=subscriber).start()
+threading.Thread(target=subscriber, args=(porta)).start()
 
 # Iniciar o publicador
-threading.Thread(target=publisher).start()
+threading.Thread(target=publisher, args=(porta, nick)).start()
 
 
 
